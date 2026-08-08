2e7e341a7d3018282c3f871f236d4b2079948cb4
Larisa Bekasova <127313972+labekasova@users.noreply.github.com>
2026-08-08 15:28:12 +0400

    Update main to v4.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './style.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+)
